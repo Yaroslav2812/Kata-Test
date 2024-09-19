741beee8fe8862177ccd3151e3b67790a5b86fc0
Yaroslav2812 <nimatov281204@mail.ru>
2024-09-19 23:16:40 +0300

    main.go

--- main.go
+++ main.go
@@ -8,53 +8,39 @@ import (
 )
 
 func main() {
-	allRomanArabic := make(map[string]int)
-	allRomanArabic["I"] = 1
-	allRomanArabic["II"] = 2
-	allRomanArabic["III"] = 3
-	allRomanArabic["IV"] = 4
-	allRomanArabic["V"] = 5
-	allRomanArabic["VI"] = 6
-	allRomanArabic["VII"] = 7
-	allRomanArabic["VIII"] = 8
-	allRomanArabic["IX"] = 9
-	allRomanArabic["X"] = 10
-	allRomanArabic["1"] = 1
-	allRomanArabic["2"] = 2
-	allRomanArabic["3"] = 3
-	allRomanArabic["4"] = 4
-	allRomanArabic["5"] = 5
-	allRomanArabic["6"] = 6
-	allRomanArabic["7"] = 7
-	allRomanArabic["8"] = 8
-	allRomanArabic["9"] = 9
-	allRomanArabic["10"] = 10
+	var allRomanArabic = map[string]int{
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5,
+		"6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
+		"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5,
+		"VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
+	}
 	/* у меня римские и арабские цифры были записаны в тип string по этому я "1" = 1 тоже конвертирую
 	возможно это можно было сделать как-то проще, но я понял как это сделать таким образом
 	*/
 
-	convArabicToRoman := make(map[int]string)
-	convArabicToRoman[1] = "I"
-	convArabicToRoman[2] = "II"
-	convArabicToRoman[3] = "III"
-	convArabicToRoman[4] = "IV"
-	convArabicToRoman[5] = "V"
-	convArabicToRoman[6] = "VI"
-	convArabicToRoman[7] = "VII"
-	convArabicToRoman[8] = "VIII"
-	convArabicToRoman[9] = "IX"
-	convArabicToRoman[10] = "X"
-	convArabicToRoman[11] = "XI"
-	convArabicToRoman[12] = "XII"
-	convArabicToRoman[13] = "XIII"
-	convArabicToRoman[14] = "XIV"
-	convArabicToRoman[15] = "XV"
-	convArabicToRoman[16] = "XVI"
-	convArabicToRoman[17] = "XVII"
-	convArabicToRoman[18] = "XVIII"
-	convArabicToRoman[19] = "XIX"
-	convArabicToRoman[20] = "XX"
-
+	var convArabicToRoman = map[int]string{
+		1: "I", 2: "II", 3: "III", 4: "IV",
+		5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX",
+		10: "X", 11: "XI", 12: "XII", 13: "XIII", 14: "XIV",
+		15: "XV", 16: "XVI", 17: "XVII", 18: "XVIII", 19: "XIX",
+		20: "XX", 21: "XXI", 22: "XXII", 23: "XXIII", 24: "XXIV",
+		25: "XXV", 26: "XXVI", 27: "XXVII", 28: "XXVIII", 29: "XXIX",
+		30: "XXX", 31: "XXXI", 32: "XXXII", 33: "XXXIII", 34: "XXXIV",
+		35: "XXXV", 36: "XXXVI", 37: "XXXVII", 38: "XXXVIII", 39: "XXXIX",
+		40: "XL", 41: "XLI", 42: "XLII", 43: "XLIII", 44: "XLIV",
+		45: "XLV", 46: "XLVI", 47: "XLVII", 48: "XLVIII", 49: "XLIX",
+		50: "L", 51: "LI", 52: "LII", 53: "LIII", 54: "LIV",
+		55: "LV", 56: "LVI", 57: "LVII", 58: "LVIII", 59: "LIX",
+		60: "LX", 61: "LXI", 62: "LXII", 63: "LXIII", 64: "LXIV",
+		65: "LXV", 66: "LXVI", 67: "LXVII", 68: "LXVIII", 69: "LXIX",
+		70: "LXX", 71: "LXXI", 72: "LXXII", 73: "LXXIII", 74: "LXXIV",
+		75: "LXXV", 76: "LXXVI", 77: "LXXVII", 78: "LXXVIII", 79: "LXXIX",
+		80: "LXXX", 81: "LXXXI", 82: "LXXXII", 83: "LXXXIII", 84: "LXXXIV",
+		85: "LXXXV", 86: "LXXXVI", 87: "LXXXVII", 88: "LXXXVIII", 89: "LXXXIX",
+		90: "XC", 91: "XCI", 92: "XCII", 93: "XCIII", 94: "XCIV",
+		95: "XCV", 96: "XCVI", 97: "XCVII", 98: "XCVIII", 99: "XCIX",
+		100: "C",
+	}
 	println("Добро пожаловать в калькулятор")
 
 	reader := bufio.NewReader(os.Stdin)
